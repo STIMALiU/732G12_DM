bccee2a2a33e1ec10fb5f8642a33b51f25f0f096
josefwilzen <josefwilzen@gmail.com>
2021-09-15 10:25:21 +0200

    class evaluation for keras models

---
+++ labs/class_evaluation_keras.R
@@ -0,0 +1,43 @@
+# new_data - new data for prediction
+# model    - a keras model
+# digits   - number of digits used 
+
+class_evaluation_keras <- function(new_data, model, true_y, digits = 3){
+  # Predikterar klassen för new_data givet den skattade modellen
+  browser()
+  class_pred <- model %>% predict(new_data) %>% k_argmax()
+  pred<-as.vector(as.array(class_pred))
+
+  # Konverterar de predikterade klasserna till en faktor med samma nivåer som de sanna klasserna
+  pred <- factor(pred, levels = levels(factor(true_y)))
+  
+  # Skapar förväxlingsmatris med rader som indikerar sanna klassen och kolumnen som indikerar predikterade klassen
+  confusion <- table(true_y, pred)
+  
+  # Träffsäkerhet är antalet korrekta prediktioner dividerat med antalet observationer
+  accuracy <- sum(diag(confusion)) / sum(confusion)
+  
+  # Felkvoten är 1 - träffsäkerheten
+  misclass <- 1 - accuracy
+  
+  # Sensitivitet är antalet korrekta prediktioner AV DEN NUVARANDE KLASSEN dividerat med antalet observationer AV DEN NUVARANDE KLASSEN
+  sensitivity <- diag(confusion) / rowSums(confusion) 
+  
+  # Specificitet är antalet korrekta prediktioner AV ICKE DEN NUVARANDE KLASSEN dividerat med antalet observationer AV ICKE DEN NUVARANDE KLASSEN
+  specificity <- NULL
+  for(i in 1:nrow(confusion)){
+    specificity[i] <- sum(confusion[-i, -i])/sum(confusion[-i, ])
+  }
+  
+  # Sammanställer alla resultat i en egen lista som sedan returneras
+  evaluation <- list(confusion_matrix = confusion, 
+                     overall = 
+                       cbind(acc = accuracy, 
+                             mis = misclass),
+                     class_wise = 
+                       rbind(sensitivity = round(sensitivity, digits = digits),
+                             specificity = round(specificity, digits = digits))
+  )
+  
+  return(evaluation)
+}
